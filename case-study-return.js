5f1df4907c82c8e86e5e0796d7fe6bc96a5ba653
NivedithaDudyala <dudyalaniveditha@gmail.com>
2026-08-11 10:29:48 -0700

    Fix case study back navigation

---
+++ case-study-return.js
@@ -0,0 +1,23 @@
+(() => {
+  const returnLink = document.querySelector('[data-role-return]');
+  if (!returnLink) return;
+
+  const source = new URLSearchParams(window.location.search).get('from');
+  const destinations = {
+    pm: '../pm/index.html#work',
+    design: '../design/index.html#work',
+    research: '../research/index.html#work'
+  };
+
+  returnLink.href = destinations[source] || '../index.html';
+
+  if (!destinations[source]) return;
+
+  const caseStudyPath = /\/(ARWell|ARWell-Pro|Shloka|TGP|PrivateAccess)\//;
+  document.querySelectorAll('a[href]').forEach((link) => {
+    const url = new URL(link.href, window.location.href);
+    if (url.origin !== window.location.origin || !caseStudyPath.test(url.pathname)) return;
+    if (!url.searchParams.has('from')) url.searchParams.set('from', source);
+    link.href = `${url.pathname}${url.search}${url.hash}`;
+  });
+})();
